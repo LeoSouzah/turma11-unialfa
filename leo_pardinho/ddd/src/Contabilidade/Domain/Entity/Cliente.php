a15b29d03cb937944be3785c48738ea469b0bdf6
unknown <lucas02.mendes@gmail.com>
2022-08-31 20:55:49 -0300

    Mexendo com docker

--- leo_pardinho/ddd/src/Contabilidade/Domain/Entity/Cliente.php
+++ leo_pardinho/ddd/src/Contabilidade/Domain/Entity/Cliente.php
@@ -6,9 +6,38 @@ final class Cliente
 {
     private string $nome;
     private string $sobrenome;
-    private string $idade;
+    private int $idade;
     protected string $cpf;
-    protected string $dadosReceitaFederal;
-    private array $boletoEmAtraso;
+    protected array $dadosReceitaFederal;
+    private array $boletosEmAtraso;
     private array $boletos;
+
+    public function __construct(string $nome, string $sobrenome, int $idade, string $cpf)
+    {
+        
+    }
+
+
+    public function setNome($nome){
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function setSobrenome($sobrenome){
+        $this->sobrenome = $sobrenome;
+
+        return $this;
+    }
+    public function setIdade($idade){
+        $this->idade = $idade;
+
+        return $this;
+    }
+
+    public function setCpf($cpf){
+        $this->cpf = $cpf;
+
+        return $this;
+    }
 }
