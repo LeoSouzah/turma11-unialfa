4de84b03c28f383902b5799d3615bfb051360d15
unknown <lucas02.mendes@gmail.com>
2022-08-31 22:17:55 -0300

    Docker

--- leo_pardinho/ddd/src/Contabilidade/Domain/Entity/Cliente.php
+++ leo_pardinho/ddd/src/Contabilidade/Domain/Entity/Cliente.php
@@ -4,38 +4,56 @@ namespace App\Contabilidade\Domain\Entity;
 
 final class Cliente
 {
-    private string $nome;
-    private string $sobrenome;
-    private int $idade;
-    protected string $cpf;
     protected array $dadosReceitaFederal;
     private array $boletosEmAtraso;
     private array $boletos;
 
-    public function __construct(string $nome, string $sobrenome, int $idade, string $cpf)
+    public function __construct(
+        private string $nome,
+        private string $sobrenome,
+        private int $idade,
+        protected string $cpf
+    )
     {
-        
+        $this->setNome($nome);
+        $this->setSobrenome($sobrenome);
+        $this->setIdade($idade);
+        $this->setCpf($cpf);
     }
 
+    public function buscaDadosReceitaFederal()
+    {
+        return [];
+    }
 
-    public function setNome($nome){
+    public function buscaRelatorioBoletos()
+    {
+        return [];
+    }
+
+    public function setNome($nome)
+    {
         $this->nome = $nome;
 
         return $this;
     }
 
-    public function setSobrenome($sobrenome){
+    public function setSobrenome($sobrenome)
+    {
         $this->sobrenome = $sobrenome;
 
         return $this;
     }
-    public function setIdade($idade){
+
+    public function setIdade($idade)
+    {
         $this->idade = $idade;
 
         return $this;
     }
 
-    public function setCpf($cpf){
+    public function setCpf($cpf)
+    {
         $this->cpf = $cpf;
 
         return $this;
